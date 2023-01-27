8b5aebd28b4cf8f7a0a341493e1129927d147b3d
khangnt <khangnt@proton.me>
2023-01-27 18:50:55 +0700

    test post

--- InstagramClone/settings.py
+++ InstagramClone/settings.py
@@ -125,21 +125,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = ''
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-LOGOUT_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = 'index'
 
-LOGIN_URL = ''
+LOGOUT_REDIRECT_URL = 'sign-in'
+
+LOGIN_URL = 'sign-in'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- InstagramClone/urls.py
+++ InstagramClone/urls.py
@@ -1,7 +1,7 @@
-"""InstagramClone URL Configuration
+"""ig_prj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+# from elements import views as e_views
+# from authusers import views as auth_views
+from authy.views import UserProfile, follow
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('authy.urls')),
     path('', include('post.urls')),
+    path('message/', include('directs.urls')),
+    path('notifications/', include('notification.urls')),
+
+    # profile
+    path('<username>/', UserProfile, name='profile'),
+    path('<username>/saved/', UserProfile, name='profilefavourite'),
+    path('<username>/follow/<option>/', follow, name='follow'),
+
 ]
+
+# This is used for
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
