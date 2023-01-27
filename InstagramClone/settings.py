b4f08b8a1770cf31fff66e5b425ee013a44af54d
khangnt <khangnt@proton.me>
2023-01-27 18:29:00 +0700

    create post app

--- InstagramClone/settings.py
+++ InstagramClone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
+    # own
+    'post',
+    'crispy_forms',
+    'authy',
+    'comment',
+    'directs',
+    'notification',
 ]
 
 MIDDLEWARE = [
@@ -54,8 +64,7 @@ ROOT_URLCONF = 'InstagramClone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +131,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = ''
+
+LOGOUT_REDIRECT_URL = ''
+
+LOGIN_URL = ''
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
