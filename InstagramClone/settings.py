f251dd7641e3bc7013fa7696f97500f72c67b0a8
khangnt <khangnt@proton.me>
2023-01-27 19:53:45 +0700

    make it better

--- InstagramClone/settings.py
+++ InstagramClone/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'InstagramClone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'khangnt',
+    'USER': 'postgres',
+    'PASSWORD': '123456',
+    'HOST':'localhost'
     }
 }
 
